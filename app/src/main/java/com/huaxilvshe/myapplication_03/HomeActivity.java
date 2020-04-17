609993b174fff44c844b11154b40f673517d47c6
二小程序猿 <892707945@qq.com>
2020-04-17 10:47:23 +0800

    新增HomeActivity布局视图

--- app/src/main/java/com/huaxilvshe/myapplication_03/HomeActivity.java
+++ app/src/main/java/com/huaxilvshe/myapplication_03/HomeActivity.java
@@ -1,14 +1,27 @@
 package com.huaxilvshe.myapplication_03;
 
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
 /**
  * Description
  * Author:华西绿舍建材有限公司 - 科技信息部
  * Date:2020/4/16 3:43 PM
  */
-public class HomeActivity {
+public class HomeActivity extends Activity {
 
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tableview);
+    }
 
     public  void getuserNameFoundct(String name, Number num){
+
+
 
     }
 }
